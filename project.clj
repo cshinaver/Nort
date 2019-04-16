40b9b1498d25938ea7d8ee1846cfc8da2d5beb5f
cshinaver <bshinaver@gmail.com>
2019-04-15 21:47:38 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-seymore "0.1.0-SNAPSHOT"
+(defproject hello-seymore "0.1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
                  [reagent "0.8.1"]]
