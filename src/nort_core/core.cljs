afa64c91499b16c466238763bf8c6dbf1cf14d5e
cshinaver <bshinaver@gmail.com>
2019-04-15 21:09:28 -0400

    Added reagent. Clojurescript elements rendering to the dom!!!

--- src/nort_core/core.cljs
+++ src/nort_core/core.cljs
@@ -1,3 +1,15 @@
-(ns nort-core.core)
+(ns nort-core.core
+  (:require [reagent.core :as r]))
 
-(.log js/console "lolwut")
+(defonce click-count (r/atom 0))
+
+(defn my-p-tag []
+  [:button
+   {:on-click
+    (fn []
+      (swap! click-count inc)
+      )}
+   (str "yooooo hit me fam: click count == " @click-count)])
+
+(r/render [my-p-tag]
+          (.-body js/document))
